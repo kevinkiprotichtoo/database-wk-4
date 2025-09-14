c4d8b3f05531385a1fba6e6181ea70e96fa288ce
kevin too <114982852+kevinkiprotichtoo@users.noreply.github.com>
2025-09-14 11:10:09 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,26 @@
+-- Question 1
+-- Show total payment amount per date, latest 5 dates only
+SELECT paymentDate, SUM(amount) AS totalAmount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+-- Question 2
+-- Find average credit limit of each customer
+SELECT customerName, country, AVG(creditLimit) AS avgCreditLimit
+FROM customers
+GROUP BY customerName, country;
+
+-- Question 3
+-- Total price of products ordered
+SELECT productCode, quantityOrdered, (quantityOrdered * priceEach) AS totalPrice
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+
+-- Question 4
+-- Highest payment amount for each check number
+SELECT checkNumber, MAX(amount) AS highestAmount
+FROM payments
+GROUP BY checkNumber;
 
